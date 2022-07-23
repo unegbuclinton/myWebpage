d48061b580d07e8bf06a611b0733eedb110ef462
Bear_bear <unegbuclinton5@gmail.com>
2022-07-23 22:51:55 +0100

    add new projects

--- src/components/pages/Work/index.jsx
+++ src/components/pages/Work/index.jsx
@@ -2,105 +2,77 @@ import React from 'react';
 import styled from 'styled-components';
 import { COLORS } from '../../../constants/colors';
 import { FONTSIZES, FONTWEIGHTS } from '../../../constants/fonts';
+import movie from '../../../img/movie.jpg';
+import myPage from '../../../img/myPage.jpg';
 import project from '../../../img/project.png';
-import shop from '../../../img/shop.png';
 
 import Task from '../../../img/Task.png';
 import Button from '../../atoms/Button';
 
 function WorkPage() {
+  const works = [
+    {
+      name: 'MovieBooz',
+      desc: 'MovieBooz is functional and responsive application that allows you purchase movie tickets, add payment card,edit profle,add to favourite and other interesting functionalities. Built with React, styled-component, redux-toolkit',
+      netlifyHref: 'https://movieboomz.netlify.app/',
+      githubHref: 'https://github.com/unegbuclinton/MovieApp',
+      img: movie,
+    },
+    {
+      name: 'My WebPage',
+      desc: 'This is a simple functional application that tells you about work history,project and experience. Put together with react, styled-component and react-hooks',
+      netlifyHref: 'https://movieboomz.netlify.app/',
+      githubHref: 'https://github.com/unegbuclinton/TaskTracker',
+      img: myPage,
+    },
+    {
+      name: 'Smart-Banking',
+      desc: 'SmartBanking is a static web application built using react and styled component',
+      netlifyHref: 'https://smartbank.netlify.app/',
+      githubHref: 'https://github.com/unegbuclinton/Web-communication',
+      img: project,
+    },
+    {
+      name: 'Task Tracker',
+      desc: 'Task Tracker is a simple tracking application that allow you add, remove and toggle task/events, using simple react hooks and react router',
+      netlifyHref: 'https://trackt.netlify.app/',
+      githubHref: 'https://github.com/unegbuclinton/TaskTracker',
+      img: Task,
+    },
+  ];
   return (
     <WorkWrapper>
       <h1 className="work-header">
         work (<span>'selected'</span> )
       </h1>
       <WorkItemContainer>
-        <div className="work-item">
-          <h2 className="work-item__header">Smart-Banking</h2>
-          <div className="work-item__1"></div>
-          <p className="work-item__description">
-            This is a static <span>Smart banking</span> web application built
-            using react and styled component
-          </p>
-          <Button className="work-item__btn" auth>
-            <a
-              className="cta-btn"
-              href="https://smartbank.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Demo
-            </a>
-          </Button>
-          <Button className="work-item__btn--github" auth>
-            <a
-              className="cta-btn"
-              href="https://github.com/unegbuclinton/Web-communication"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>
-          </Button>
-        </div>
-        <div className="work-item">
-          <h2 className="work-item__header">E-commerce Store</h2>
-          <div className="work-item__2"></div>
-          <p className="work-item__description">
-            An <span>E-store</span> application created with react and also used
-            data froma a free API. With great functionality to add and remove
-            from cart,sum up etc
-          </p>
-          <Button className="work-item__btn" auth>
-            <a
-              className="cta-btn"
-              href="https://ccart.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Demo
-            </a>
-          </Button>
-          <Button className="work-item__btn--github" auth>
-            <a
-              className="cta-btn"
-              href="https://github.com/unegbuclinton/shopCart"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>
-          </Button>
-        </div>
-        <div className="work-item">
-          <h2 className="work-item__header">Task Tracker</h2>
-          <div className="work-item__3"></div>
-          <p className="work-item__description">
-            A simple <span>Task Tracker</span> application with functionality to
-            add and remove tasks and also toogle,created using react.
-          </p>
-          <Button className="work-item__btn" auth>
-            <a
-              className="cta-btn"
-              href="https://github.com/unegbuclinton/TaskTracker"
-              target="_blank"
-              rel="noopener noreferrer"
-              a
-            >
-              Demo
-            </a>
-          </Button>
-          <Button className="work-item__btn--github" auth>
-            <a
-              className="cta-btn"
-              href="https://trackt.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>
-          </Button>
-        </div>
+        {works?.map(({ name, desc, netlifyHref, githubHref, img }) => (
+          <div className="work-item">
+            <h2 className="work-item__header">{name}</h2>
+            <img src={img} alt="" className="img" />
+            <p className="work-item__description">{desc}</p>
+            <Button className="work-item__btn" auth>
+              <a
+                className="cta-btn"
+                href={netlifyHref}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Demo
+              </a>
+            </Button>
+            <Button className="work-item__btn--github" auth>
+              <a
+                className="cta-btn"
+                href={githubHref}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </Button>
+          </div>
+        ))}
       </WorkItemContainer>
     </WorkWrapper>
   );
@@ -128,45 +100,19 @@ const WorkWrapper = styled.section`
     margin-top: 2rem;
     padding: 1.5;
 
-    .work-item__1 {
-      width: 80%;
+    .img {
+      width: 100%;
       height: 20rem;
       background-position: center;
-      background-image: url(${project});
       background-size: cover;
       border-radius: 1rem;
 
       &:hover {
-        transition: transform 0.3s ease-out;
+        transition: transform 0.3s ease-in-out;
         transform: scale(0.9);
       }
     }
-    .work-item__2 {
-      width: 80%;
-      height: 20rem;
-      background-position: center;
-      background-image: url(${shop});
-      background-size: cover;
-      border-radius: 1rem;
 
-      &:hover {
-        transition: transform 0.3s ease-out;
-        transform: scale(0.9);
-      }
-    }
-    .work-item__3 {
-      width: 80%;
-      height: 20rem;
-      background-position: center;
-      background-image: url(${Task});
-      background-size: cover;
-      border-radius: 1rem;
-
-      &:hover {
-        transition: transform 0.3s ease-out;
-        transform: scale(0.9);
-      }
-    }
     .work-item__header {
       font-size: ${FONTSIZES.base};
       color: ${COLORS.ivory};
@@ -174,6 +120,7 @@ const WorkWrapper = styled.section`
     }
 
     .work-item__description {
+      max-width: 80%;
       font-size: ${FONTSIZES.small};
       color: ${COLORS['pewter-blue']};
       margin: 1rem 0;
@@ -202,5 +149,21 @@ const WorkItemContainer = styled.div`
   .cta-btn {
     text-decoration: none;
     color: ${COLORS.white};
+  }
+  @media only screen and (min-width: 768px) {
+    .work-item {
+      .img {
+        width: 80%;
+        height: 50rem;
+        background-position: center;
+        background-size: cover;
+        border-radius: 1rem;
+
+        &:hover {
+          transition: transform 0.3s ease-in-out;
+          transform: scale(0.9);
+        }
+      }
+    }
   }
 `;
